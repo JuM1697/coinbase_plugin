c6671adb54b1731b4eae03fdc172dd4ff25ea8c4
JuM1697 <justin.michael@marquardt.de>
2019-07-18 09:24:06 +0200

    Argument handling has been added

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # coinbase_plugin
 Plugin to monitor Coinbase prices and sell rates
+
+## Requirements
+* argparse
+* coinbase 
--- check_cbSellAmount.py
+++ check_cbSellAmount.py
@@ -1,12 +1,26 @@
+import sys, argparse
 from coinbase.wallet.client import Client
 
-api_key = ''
-api_secret = ''
+parser = argparse.ArgumentParser(description='Get the current Price of Coinbase Cryptos')
+parser.add_argument("-k", "--key", required=True, help="Coinbase API Key")
+parser.add_argument("-s", "--secret", required=True, help="Coinbase API Secret")
+parser.add_argument("-p", "--paymentMethod", required=True, help="Desired Target Payment Method e.g. EUR Wallet")
+parser.add_argument("-c", "--currencyToBuy", required=True, help="Desired Target Currency to buy e.g. BTC")
+parser.add_argument("-a", "--accountWallet", required=True, help="Desired Target wallet to buy from e.g. BAT Wallet")
+parser.add_argument("-i", "--invest", required=True, help="Desired amount to invest from Payment Method e.g. 5.00")
+parser.add_argument("-b", "--buy", required=True, help="How much of the desired Crypto would you like to buy for the previously defined amount to invest? e.g. 23")
+args = vars(parser.parse_args())
 
-targetPaymentMethod = "EUR Wallet"
-targetBuyCurrencyWallet = "BAT Wallet"
-targetCurrency = "BAT"
-targetAmountToBuy = "23.64"
+
+api_key = args["key"]
+api_secret = args["secret"]
+targetPaymentMethod = args["paymentMethod"]
+targetCurrency = args["currencyToBuy"]
+targetBuyCurrencyWallet = args["accountWallet"]
+investMoney = args["invest"]
+targetAmountToBuy = args["buy"]
+
+
 
 client = Client(api_key, api_secret)
 accounts = client.get_accounts()
@@ -24,5 +38,12 @@ for account in accounts.data:
 
 
 buy = client.buy(accountID,commit="false",amount=targetAmountToBuy, currency=targetCurrency, payment_method=paymentID, quote="true")
-print(buy["total"]["amount"])
 
+costs = buy["total"]["amount"]
+
+if costs < investMoney:
+    print("Einkaufen!")
+    sys.exit(2)
+else:
+    print("Aktueller Preis: "+costs+" Warten...")
+    sys.exit(0)
